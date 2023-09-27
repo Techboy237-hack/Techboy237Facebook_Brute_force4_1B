0e9c61ed9e2e146090578b1806489c71ddb3a6e1
Techboy237 <104980530+Techboy237-hack@users.noreply.github.com>
2023-09-27 12:22:50 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,20 +1,10 @@
-import os
 import subprocess
-import importlib
 
 if __name__ == "__main__":
     # Use try-except to catch any errors during the script execution
     try:
+        # Execute the "git pull" command
         subprocess.run(["git", "pull"])
-        
-        module_name = "Techboy237Facebook_Brute_force4_1B"
-        module = importlib.import_module(module_name)
-        
-        # Check if the function 'azimvau' exists in the imported module before calling it
-        if hasattr(module, 'azimvau') and callable(module.azimvau):
-            module.azimvau()
-        else:
-            print(f"Function 'azimvau' not found in module '{module_name}'")
     except Exception as e:
+        # Handle exceptions here (e.g., print an error message)
         print(f"An error occurred: {e}")
-
